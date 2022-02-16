5d597f67af2ac1a8a38b6ca824597751768e188a
chelseaerinavery <chelsea.erin.avery@gmail.com>
2022-02-15 20:40:42 -0700

    modal functionality

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faCircle } from "@fortawesome/free-regular-svg-icons";
 
+const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
+
 function App() {
   const [todoData, setTodoData] = useState(null);
   const [formData, setFormData] = useState({ name: "", description: "" });
+  const [modalVisible, setModalVisible] = useState(false);
+
   useEffect(() => {
     if (!todoData) {
       getToDos();
@@ -18,13 +22,13 @@ function App() {
   });
 
   const getToDos = async () => {
-    const response = await fetch(`http://localhost:3000/todo`);
+    const response = await fetch(`${API_ENDPOINT}/todo`);
     var newTodos = await response.json();
     setTodoData(newTodos);
   };
 
   const handleSubmit = async (values) => {
-    await fetch("http://localhost:3000/todo", {
+    await fetch(`${API_ENDPOINT}/todo`, {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -33,6 +37,7 @@ function App() {
       body: JSON.stringify(formData),
     });
     getToDos();
+    modalDisplay(false);
   };
 
   const handleChange = (e) => {
@@ -41,7 +46,7 @@ function App() {
   };
 
   const deleteTodo = async (id) => {
-    await fetch("http://localhost:3000/todo", {
+    await fetch(`${API_ENDPOINT}/todo`, {
       method: "DELETE",
       headers: {
         Accept: "application/json",
@@ -53,7 +58,7 @@ function App() {
   };
 
   const completeTodo = async (id, completed) => {
-    await fetch("http://localhost:3000/todo", {
+    await fetch(`${API_ENDPOINT}/todo`, {
       method: "PUT",
       headers: {
         Accept: "application/json",
@@ -64,28 +69,41 @@ function App() {
     getToDos();
   };
 
+  const modalDisplay = (isDisplayed) => {
+    setModalVisible(isDisplayed);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>To-Do:</h2>
-        <button id="add">+</button>
+        <button id="add" onClick={() => modalDisplay(true)}>
+          +
+        </button>
       </header>
-      <form className="App-form" onSubmit={handleSubmit}>
-        <p>Title:</p>
-        <input
-          value={formData.name}
-          type="text"
-          name="name"
-          onChange={handleChange}
-        ></input>
-        <p>Decription:</p>
-        <textarea
-          value={formData.description}
-          onChange={handleChange}
-          name="description"
-        ></textarea>
-        <button type="submit">Add</button>
-      </form>
+
+      {modalVisible && (
+        <form className="App-form" onSubmit={handleSubmit}>
+          <p>Title:</p>
+          <input
+            value={formData.name}
+            type="text"
+            name="name"
+            onChange={handleChange}
+          ></input>
+          <p>Decription:</p>
+          <textarea
+            value={formData.description}
+            onChange={handleChange}
+            name="description"
+          ></textarea>
+          <button id="cancel" onClick={() => modalDisplay(false)}>
+            Cancel
+          </button>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+
       <div className="flex">
         {todoData &&
           todoData.map((toDo) => {
